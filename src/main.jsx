c35ef485cd7d2eb8cceb5ad9747c8ef94d2f2309
username <133022217+AniketShrivastava@users.noreply.github.com>
2023-10-19 01:35:06 +0530

    added axios instance config setup

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,12 @@ import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
+  <Provider store={store}>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-        <Toaster/>
-      </Provider>
-
+      <App />
+      <Toaster />
     </BrowserRouter>
-  
+  </Provider>
+
+
 )
